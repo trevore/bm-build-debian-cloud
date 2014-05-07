715299fa86c44c163e26b52b0b2dcfd83098578c
Trevor Ellermann <trevor@ellermann.net>
2014-05-07 19:51:25 +0000

    Take out minor version

--- bm-build.sh
+++ bm-build.sh
@@ -1,1 +1,1 @@
-./build-debian-cloud ec2 --volume-size 10 --filesystem ext4 --plugin plugins/admin-user --description 'BM Debian 7.1 (Wheezy) Base' --codename wheezy
+./build-debian-cloud ec2 --volume-size 10 --filesystem ext4 --plugin plugins/admin-user --description 'BM Debian 7 (Wheezy) Base' --codename wheezy
